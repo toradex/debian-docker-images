855c148e73b039b463fcdc3172bd2c1524965a7b
Stefan Agner <stefan.agner@toradex.com>
2019-01-29 15:01:38 +0100

    initial commit for Weston container
    Add Weston container based on the Debian base container. The
    container can be used to start Weston as well as to start
    Wayland clients:
      docker run -d -it --restart=always --privileged -v /dev:/dev \
        -v /tmp:/tmp agners/debian-weston:buster \
        weston-launch --tty=/dev/tty7 --user=root
      docker run -d -it --restart=always -v /tmp:/tmp \
        agners/debian-weston:buster /usr/bin/weston-terminal
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- build.sh
+++ build.sh
@@ -4,13 +4,16 @@ REPOSITORY=torizon
 ARCH=arm32v7
 
 IMAGE_BASE=${REPOSITORY}/${ARCH}-debian-base:buster
+IMAGE_WESTON=${REPOSITORY}/${ARCH}-debian-weston:buster
 
 case "$1" in
 "build")
 	docker build -f base/Dockerfile -t ${IMAGE_BASE} base/
+	docker build -f weston/Dockerfile -t ${IMAGE_WESTON} weston/
 	;;
 "deploy")
 	docker push ${IMAGE_BASE}
+	docker push ${IMAGE_WESTON}
 	;;
 *)
 	echo "Use \"build\" or \"deploy\" as argument."
