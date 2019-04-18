d5a9cd44872a70d20e106cb5226b68ac764004e0
Valter Minute <valter.minute@toradex.com>
2019-04-18 08:24:09 +0200

    add image with wayland-client libraries ready to use
    add an image that can be used to integrate wayland client applications with
    our current weston server container.
    
    Signed-off-by: Valter Minute <valter.minute@toradex.com>

--- build.sh
+++ build.sh
@@ -5,15 +5,18 @@ ARCH=arm32v7
 
 IMAGE_BASE=${REPOSITORY}/${ARCH}-debian-base:buster
 IMAGE_WESTON=${REPOSITORY}/${ARCH}-debian-weston:buster
+IMAGE_WAYLAND_CLIENT=${REPOSITORY}/${ARCH}-debian-wayland-client:buster
 
 case "$1" in
 "build")
 	docker build -f base/Dockerfile -t ${IMAGE_BASE} base/
 	docker build -f weston/Dockerfile -t ${IMAGE_WESTON} weston/
+	docker build -f wayland-client/Dockerfile -t ${IMAGE_WAYLAND_CLIENT} weston/
 	;;
 "deploy")
 	docker push ${IMAGE_BASE}
 	docker push ${IMAGE_WESTON}
+    docker push ${IMAGE_WAYLAND_CLIENT}
 	;;
 *)
 	echo "Use \"build\" or \"deploy\" as argument."
