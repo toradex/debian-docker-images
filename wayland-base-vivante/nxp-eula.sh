9be0e5824c98ee4e375ff38e965e0e6025f93b74
Stefan Agner <stefan.agner@toradex.com>
2019-10-10 17:11:37 +0200

    vivante-eula.sh: only print EULA on interactive login shells
    Only print EULA on an interactive login shell. This prevents the
    Weston container from printing the complete EULA to the output but
    prints a hint what the user should do.
    
    Also use a login shell by default in the wayland-base-vivante
    container.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- wayland-base-vivante/nxp-eula.sh
+++ wayland-base-vivante/nxp-eula.sh
@@ -1,16 +1,27 @@
-#!/bin/bash
+if [ -f "/tmp/nxp-eula-accepted" ]; then
+    echo "NXP EULA has already been accepted."
+elif [ ! -n "$BASH" ]; then
+    # Interactive shell detection is bashism. Do not bother if we cannot detect.
+    echo "WARNING: NXP EULA has been auto-accepted; this imply you agree with it anyway."
+elif [ "${ACCEPT_FSL_EULA}" != "1" ]; then
+    cat <<EOMESSAGE
+This container uses Vivante binary drivers provided by NXP. You need to read
+and accept the NXP EULA before continuing..
 
-# Only prompt for Vivante EULA if Vivante packages are present
-dpkg-query -l "*vivante*"  > /dev/null 2>&1
+EOMESSAGE
 
-if [ $? = 0 ]; then
-    if [ ! "${ACCEPT_FSL_EULA}" = "1" ]; then
-        echo "You need to read and accept the Vivante EULA before continuing.."
-        echo
+    # In case we run non-interactively, ask the user to accept through environment
+    if [[ $- != *i* ]]; then
+         cat <<EOMESSAGE
+Start an interactive shell to read the NXP EULA. Alternatively set the
+environment variable ACCEPT_FSL_EULA=1 to accept the EULA non-interacitvely.
+EOMESSAGE
+        exit 1
+    fi
 
-        sleep 2
+    sleep 2
 
-        cat | more -d <<EOEULA
+    cat | more -d <<EOEULA
 LA_OPT_NXP_Software_License v5 January 2019
 IMPORTANT.  Read the following NXP Software License Agreement ("Agreement")
 completely. By selecting the "I Accept" button at the end of this page, or by
@@ -568,23 +579,17 @@ within, Authorized Systems that include a Vivante Graphics Processing Unit.
 
 EOEULA
 
-        echo -n "Do you accept the Vivante EULA you just read? (y/N) "
-        read REPLY
-        if [ "$REPLY" != "y" ] && [ "$REPLY" != "Y" ]; then
-            echo "Vivante EULA has not been accepted..."
-            exit 1
-        fi
-
-        echo "Vivante EULA has been accepted."
-        echo
-
-        # Further login shells should not need to accept eula again
-        NL=$(awk '/vivante-eula.sh/ {print FNR}' /etc/profile)
-        sed -i ${NL%% *}'iexport ACCEPT_FSL_EULA=1' /etc/profile
-    else
-        echo "WARNING: EULA has been auto-accepted; this imply you agree with it anyway."
-        NL=$(awk '/vivante-eula.sh/ {print FNR}' /etc/profile)
-        sed -i ${NL%% *}'iexport ACCEPT_FSL_EULA=1' /etc/profile
+    echo -n "Do you accept the Vivante EULA you just read? (y/N) "
+    read REPLY
+    if [ "$REPLY" != "y" ] && [ "$REPLY" != "Y" ]; then
+        echo "NXP EULA has not been accepted..."
+        exit 1
     fi
+
+    echo "NXP EULA has been accepted."
+    echo
 fi
 
+# If we get here, EULA has been auto-accepted or interactively accepted.
+# Further login shells should not need to accept EULA again
+touch /tmp/nxp-eula-accepted
