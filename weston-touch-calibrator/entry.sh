3e92e79aa5313e2b36bf62b4559a7ac1535e04ed
Stefan Eichenberger <stefan.eichenberger@toradex.com>
2020-07-09 16:12:15 +0200

    weston-touch-calibrator: entry.sh: make it work for iMX7
    Because the iMX7 doesn't have a GPU we need to use pixman instead of the
    normal GPU drm backend.
    
    Signed-off-by: Stefan Eichenberger <stefan.eichenberger@toradex.com>

--- weston-touch-calibrator/entry.sh
+++ weston-touch-calibrator/entry.sh
@@ -1,7 +1,28 @@
 #!/bin/bash -l
 
 # Use Display from argument list or DPI-1 if not specified
-DISPLAY=${1:-DPI-1}
+DISPLAY=""
+WESTON_ARGS=""
+
+function check_gpu()
+{
+    if [ ! -f /sys/devices/soc0/soc_id ]; then
+        echo "Could not detect SoC, assuming GPU is available."
+        return
+    fi
+
+    case $(cat /sys/devices/soc0/soc_id) in
+        i.MX6ULL|i.MX7S|i.MX7D)
+            echo "SoC without GPU detected, using Pixman renderer."
+            WESTON_ARGS="${WESTON_ARGS} --use-pixman"
+            DISPLAY=${DISPLAY:-Unknown-1}
+            echo "DISPLAY: $DISPLAY"
+            ;;
+        *)
+            DISPLAY=${DISPLAY:-DPI-1}
+            ;;
+    esac
+}
 
 export XDG_RUNTIME_DIR=/tmp/weston-xdg
 mkdir -p $XDG_RUNTIME_DIR
@@ -12,7 +33,8 @@ rm -f $XDG_RUNTIME_DIR/*
 echo XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR >> /etc/environment
 
 # Start weston and fork to background
-weston-launch --tty=/dev/tty7 --user=root &
+check_gpu
+weston-launch --tty=/dev/tty7 --user=root -- ${WESTON_ARGS} &
 WESTON_SERVER=$!
 
 # Wait up to 5 seconds until weston starts
@@ -20,6 +42,6 @@ for i in seq 1 5; do test -e $XDG_RUNTIME_DIR/wayland-0 && break; sleep 1; done
 test -e $XDG_RUNTIME_DIR/wayland-0 || { echo "Weston did not start"; exit 1; }
 
 # Start weston touch calibrator
-weston-touch-calibrator DPI-1
+weston-touch-calibrator ${DISPLAY}
 # Bring weston to foreground again to allow verifying the settings
 wait $WESTON_SERVER
