b1802711c64417ce18f3992eb824348d9841be49
Rafael Lorandi <rafael.lorandi@toradex.com>
2020-09-22 18:48:31 -0300

    Drop weston-touch-calibration containers

--- weston-touch-calibrator/save-calibration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Store the transformation arguments for the resistive touchscreen as udev rule
-echo 'SUBSYSTEM=="input", KERNEL=="event[0-9]*", ENV{ID_INPUT_TOUCHSCREEN}=="1", ENV{LIBINPUT_CALIBRATION_MATRIX}="'$2 $3 $4 $5 $6 $7'"' >> /etc/udev/rules.d/touchscreen.rules
-
